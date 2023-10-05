9f9525aad6c87f40bb9f3385b025074a9970da61
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-05 08:19:42 +0300

    [fix] ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://astronaut.github.io',
-  base: '/revolution5pro',
+  site: 'https://fedeparrilla10.github.io',
+  base: '/testingrepo',
 });

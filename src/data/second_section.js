f13f2368f0ac92e24045a4ad71d558b7fedc8910
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-09 20:30:45 +0300

    [add] data files for each section

--- src/data/second_section.js
+++ src/data/second_section.js
@@ -1,4 +1,4 @@
-export const textColumns = [
+export const secondSectionData = [
   {
     title: 'INNOVACIÓN',
     description:
@@ -8,5 +8,12 @@ export const textColumns = [
     title: 'NEXTGEN',
     description:
       'Diseñado para jugadores profesionales en busca de rendimiento. Supera todas las expectativas con su tecnología optimizada para la PS5. Disfruta de una interfaz de configuración enriquecida, un diseño modular mejorado y ergonómico, todo envuelto en materiales de primera calidad para un confort incomparable.',
+  },
+];
+
+export const secondSectionImg = [
+  {
+    src: '/testingrepo/innovationsection.png',
+    alt: 'Mando Revolution 5 PRO',
   },
 ];

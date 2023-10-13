fe59a8b75d7bc12253b0607531498e4d14fc74fb
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-13 09:01:18 +0300

    [add] specs + footer section

--- src/data/buy_section.js
+++ src/data/buy_section.js
@@ -1,9 +1,7 @@
-export const buySectionData = [
-  {
-    background: '/testingrepo/innovationbg.jpg',
-    price: '229.90 €',
-  },
-];
+export const buySectionData = {
+  background: '/testingrepo/colors.png',
+  price: '229.90 €',
+};
 
 export const buySectionStores = [
   {
@@ -20,5 +18,20 @@ export const buySectionStores = [
     id: 3,
     name: 'Fnac',
     src: '/testingrepo/fnac.png',
+  },
+];
+
+export const productData = [
+  {
+    imgSrc: '/testingrepo/black2.png',
+    title: 'TRIPLE BLACK',
+    logoSrc: '/testingsecond/pc.svg',
+    class: 'w-10 h-10',
+  },
+  {
+    imgSrc: '/testingrepo/white2.png',
+    title: 'BLACK & WHITE',
+    logoSrc: '/testingsecond/xbox.svg',
+    class: 'w-14 h-14',
   },
 ];

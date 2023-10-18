5dcb169ba9408a6773797c01748009b1a164ae92
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-18 19:15:36 +0300

    [add] specs for rev5pro

--- src/data/specifications.js
+++ src/data/specifications.js
@@ -23,78 +23,91 @@ export const specsImgs = [
 
 export const firstTable = [
   {
-    title: 'Acoplamiento al oído',
-    description: 'Circumaural',
-  },
-  {
-    title: 'Color',
-    description: 'Negro',
-  },
-  {
-    title: 'Peso',
-    description: '290 g',
+    title: 'Conexión',
+    description:
+      'Clave USB RF, cable USB-C / USB-A desmontable, conexión Bluetooth 5.2 con auriculares de audio',
   },
   {
     title: 'Inalámbrico',
     description: 'Sí',
   },
   {
-    title: 'Frecuencia de los auriculares',
-    description: '20 Hz - 20 kHz',
+    title: 'Con cable',
+    description: 'Sí',
   },
   {
-    title: 'Impedancia',
-    description: '32 ohms',
+    title: 'Software',
+    description: 'Sí',
   },
   {
-    title: 'Sensibilidad de los auriculares',
-    description: '111 dBSPL/V',
+    title: 'Conector de auriculares',
+    description: 'Sí',
   },
   {
-    title: '3D Audio',
-    description: `PC/XBOX - Dolby Atmos 3D Audio PS4/PS5 - PlayStation 3D Audio`,
+    title: 'Funda de transporte',
+    description: 'Sí',
   },
   {
-    title: 'Controlador de los auriculares',
-    description: '40mm / Resonadores de baja frecuencia',
+    title: 'Posición de joysticks',
+    description: 'Asimétricos',
+  },
+  {
+    title: 'Perfiles',
+    description: 'Sí',
+  },
+  {
+    title: 'Perfiles incoporados',
+    description: '4 por plataforma',
+  },
+  {
+    title: 'Compatibilidad',
+    description: 'PS5/PS4/Windows',
   },
 ];
 
 export const secondTable = [
   {
-    title: 'Frecuencia de respuesta del micrófono',
-    description: '100 Hz - 10 kHz',
+    title: 'Fecha de lanzamiento',
+    description: '12/10/2023',
   },
   {
-    title: 'Micrófono extraíble',
-    description: 'No',
+    title: 'Botones configurables',
+    description: 'Sí',
   },
   {
-    title: 'Sensibilidad del micrófono',
-    description: '45 dBV',
+    title: 'Gatillos configurables',
+    description: 'Sí',
   },
   {
-    title: 'Patrón de captación del micrófono',
-    description: 'Unidireccional',
+    title: 'Joysticks configurables',
+    description: 'Sí',
   },
   {
-    title: 'Relación señal micrófono',
-    description: '>42 dB',
+    title: 'Botón multifunción',
+    description: 'Gestión del sonido, luces y mapeo de botones',
   },
   {
-    title: 'Conectores',
-    description: 'Estación base multifunción\nAdaptador inalámbrico USB A',
+    title: 'Pad direccional',
+    description: '2 formatos',
+  },
+  {
+    title: 'Atajos',
+    description: 'Sí',
   },
   {
     title: 'Control de volumen',
-    description: 'Rueda de volumen en la oreja',
+    description: 'Sí',
   },
   {
-    title: 'Longitud del cable de la base de carga',
-    description: '90 cm',
+    title: 'Indicador LED de jugador	',
+    description: 'Sí',
   },
   {
-    title: 'Garantía',
-    description: '3 años',
+    title: 'Peso ajustable',
+    description: 'Sí',
+  },
+  {
+    title: 'Duración de la batería',
+    description: 'Más de 10 horas',
   },
 ];

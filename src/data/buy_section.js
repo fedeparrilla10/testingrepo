3f1c1b882d73573d5fcdfcb9cb6988ef2c20e72e
fedeparrilla10 <federicoivanpd@gmail.com>
2023-11-14 20:26:26 +0200

    [update] final section + favicon

--- src/data/buy_section.js
+++ src/data/buy_section.js
@@ -3,35 +3,72 @@ export const buySectionData = {
   price: '229.90 €',
 };
 
-export const buySectionStores = [
-  {
-    id: 1,
-    name: 'Game',
-    src: '/testingrepo/game.png',
-  },
-  {
-    id: 2,
-    name: 'El Corte Inglés',
-    src: '/testingrepo/corteinglés.png',
-  },
-  {
-    id: 3,
-    name: 'Fnac',
-    src: '/testingrepo/fnac.png',
-  },
-];
-
 export const productData = [
   {
     imgSrc: '/testingrepo/black2.png',
     title: 'TRIPLE BLACK',
     logoSrc: '/testingsecond/pc.svg',
     class: 'w-10 h-10',
+    buySectionStores: [
+      {
+        id: 1,
+        name: 'Game',
+        src: '/testingrepo/game.png',
+        link: 'https://www.game.es/mando-revolution-5-pro-negro-playstation-5-224299',
+      },
+    ],
   },
   {
     imgSrc: '/testingrepo/white2.png',
     title: 'BLACK & WHITE',
     logoSrc: '/testingsecond/xbox.svg',
     class: 'w-14 h-14',
+
+    buySectionStores: [
+      {
+        id: 1,
+        name: 'Coming Soon',
+        src: '/testingrepo/coming-soon.png',
+        link: '#',
+      },
+    ],
   },
 ];
+
+// export const buySectionData = {
+//   background: '/testingrepo/colors.png',
+//   price: '229.90 €',
+// };
+
+// export const buySectionStores = [
+//   {
+//     id: 1,
+//     name: 'Game',
+//     src: '/testingrepo/game.png',
+//   },
+//   {
+//     id: 2,
+//     name: 'El Corte Inglés',
+//     src: '/testingrepo/corteinglés.png',
+//   },
+//   {
+//     id: 3,
+//     name: 'Fnac',
+//     src: '/testingrepo/fnac.png',
+//   },
+// ];
+
+// export const productData = [
+//   {
+//     imgSrc: '/testingrepo/black2.png',
+//     title: 'TRIPLE BLACK',
+//     logoSrc: '/testingsecond/pc.svg',
+//     class: 'w-10 h-10',
+//   },
+//   {
+//     imgSrc: '/testingrepo/white2.png',
+//     title: 'BLACK & WHITE',
+//     logoSrc: '/testingsecond/xbox.svg',
+//     class: 'w-14 h-14',
+//   },
+// ];

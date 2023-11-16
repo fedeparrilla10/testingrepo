b10f6efc9c4c555515b87bbb27145b49f053e74a
fedeparrilla10 <federicoivanpd@gmail.com>
2023-11-16 21:10:06 +0200

    [add] 3 new videos + changes

--- src/data/first_section.js
+++ src/data/first_section.js
@@ -1,9 +1,13 @@
 export const firstSectionData = [
   {
     title: 'El primer mando Revolution Pro compatible con..',
+    mainImg: {
+      src: '/testingrepo/ps5.svg',
+      alt: 'Compatible con PS5',
+    },
     firstImg: {
-      src: '/testingrepo/ps5ps4pc.png',
-      alt: 'Compatible con PS5, PS4 y PC',
+      src: '/testingrepo/ps4pc.png',
+      alt: 'Compatible con PS4 y PC',
     },
     secondImg: {
       src: '/testingrepo/frontalblack.png',
--- src/data/video.js
+++ src/data/video.js
@@ -2,19 +2,43 @@ export const videoData = [
   {
     title: 'Tecnología Hall-Effect',
     description:
-      'Los joysticks y gatillos cuentan con una precisión y una durabilidadmuy mejoradas gracias a la tecnología magnética, que elimina la desviación de los joysticks',
-    src: '/testingrepo/analog.mp4',
+      'Por primera vez en PlayStation 5™, disfruta de los beneficios de la tecnología Hall Effect magnética. Reduce el "drift" del joystick en comparación con los anteriores mandos Revolution de Nacon. Disfruta de joysticks y gatillos más fiables y duraderos con el doble de vida útil.',
+    src: '/testingrepo/halleffect.mp4',
   },
   {
     title: 'Trigger Blocker',
     description:
-      'Adáptate a cualquier juego o situación personalizando la amplitud de tus gatillos',
-    src: '/testingrepo/r2l2trigger.mp4',
+      'Personaliza la sensibilidad de los gatillos para adaptar la cantidad de presión requerida para su activación, brindándote mayor control y comodidad al jugar.',
+    src: '/testingrepo/triggerblocker.mp4',
   },
   {
     title: 'Personalización TOTAL',
     description:
-      'Cuatro perfiles guardados por plataforma, tres sets de pesos, tres tamaños de joystick, tres sets de cabezales de joystick, una entrada de micro externo y no menos de sesenta opciones de personalización',
-    src: '/testingrepo/customization.mp4',
+      'Cuatro perfiles guardados por plataforma, tres sets de pesos, tres tamaños de joystick, tres sets de cabezales de joystick, una entrada de micro externo y no menos de 60 opciones de personalización',
+    src: '/testingrepo/personalizacion.mp4',
+  },
+  {
+    title: 'Atajos Traseros',
+    description:
+      'Las cuatro paletas traseras equipadas con microinterruptores Omron ofrecen respuestas rápidas y una experiencia de juego incomparable en términos de sensación y precisión.',
+    src: '/testingrepo/traseros.mp4',
+  },
+  {
+    title: 'Conectividad Bluetooth',
+    description:
+      'Conectividad inalámbrica multipunto: conecta el controlador a tu consola y cambia fácilmente entre el audio de tu consola y tus dispositivos Bluetooth® de audio al instante, sin complicaciones.',
+    src: '/testingrepo/bluetooth.mp4',
+  },
+  {
+    title: 'Con Cable / Sin Cable',
+    description:
+      'Conectividad con un cable de 3 metros o de forma inalámbrica con latencia optimizada mediante tecnología RF con su dongle USB.',
+    src: '/testingrepo/cableinalambrico.mp4',
+  },
+  {
+    title: 'Aplicación Propia',
+    description:
+      'Cuatro perfiles guardados por plataforma, tres sets de pesos, tres tamaños de joystick, tres sets de cabezales de joystick, una entrada de micro externo y no menos de 60 opciones de personalización',
+    src: '/testingrepo/aplicacion.mp4',
   },
 ];
